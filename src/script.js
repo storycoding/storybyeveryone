46a7ed81556b03b8b9ebf6f0ec3b4705efa35df2
Nuno Neves <youknownuno@gmail.com>
2018-03-05 21:48:46 -0800

    replaced alert with thank you message

--- src/script.js
+++ src/script.js
@@ -6,13 +6,17 @@ signup.addEventListener("click", ()=> {
 	const email = document.getElementById("email").value;
 
 	let newsfeed = document.getElementById("newsfeed");
+	let contact = document.getElementById("contact");
 
 	while (newsfeed.firstChild) {
     newsfeed.removeChild(newsfeed.firstChild);
 	}
 
+	while (contact.firstChild) {
+    contact.removeChild(contact.firstChild);
+	}
+
 	let thanks = document.createElement("p");
 	thanks.innerHTML = "Thank you for being a part of this journey, " + name + ".";
 	newsfeed.appendChild(thanks);
-	alert(`Thank you for being a part of this journey, ${name}.`);
 });
