40d24da20945d884f914720121fed0b7673b7e4c
Nuno Neves <youknownuno@gmail.com>
2018-03-06 10:26:09 -0800

    changed visibility of the form to none instead of removing it from the dom

--- src/script.js
+++ src/script.js
@@ -6,19 +6,19 @@ signup.addEventListener("click", ()=> {
 	const email = document.getElementById("email").value;
 
 	let newsfeed = document.getElementById("newsfeed");
-	//let contact = document.getElementById("contact");
+	let contact = document.getElementById("contact");
 
 	while (newsfeed.firstChild) {
     newsfeed.removeChild(newsfeed.firstChild);
 	}
 
-	// while (contact.firstChild) {
- //    contact.removeChild(contact.firstChild);
-	// }
+	contact.style.display = "none";
 
 	let thanks = document.createElement("p");
 	thanks.innerHTML = "Thank you for being a part of this journey, " + name + ".";
 	newsfeed.appendChild(thanks);
+
+});
 
 	/*
 	let encouragement = document.createElement("p");
@@ -30,4 +30,3 @@ signup.addEventListener("click", ()=> {
 	message.innerHTML="hello@storybyeveryone.com";
 	newsfeed.appendChild(message);
 	*/
-});
