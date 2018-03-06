10a964d8b6f7e88391a9a3613bf2c31d59b7b0ea
Nuno Neves <youknownuno@gmail.com>
2018-03-06 09:57:06 -0800

    this should fix the issue with the disconnected form

--- src/script.js
+++ src/script.js
@@ -6,15 +6,15 @@ signup.addEventListener("click", ()=> {
 	const email = document.getElementById("email").value;
 
 	let newsfeed = document.getElementById("newsfeed");
-	let contact = document.getElementById("contact");
+	//let contact = document.getElementById("contact");
 
 	while (newsfeed.firstChild) {
     newsfeed.removeChild(newsfeed.firstChild);
 	}
 
-	while (contact.firstChild) {
-    contact.removeChild(contact.firstChild);
-	}
+	// while (contact.firstChild) {
+ //    contact.removeChild(contact.firstChild);
+	// }
 
 	let thanks = document.createElement("p");
 	thanks.innerHTML = "Thank you for being a part of this journey, " + name + ".";
